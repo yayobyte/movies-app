00a374f1e14a77a2d30513c7697214232a67598e
yayo <yayobyte@gmail.com>
2024-04-17 15:43:10 +0400

    create theme

--- src/config/ThemeContext.tsx
+++ src/config/ThemeContext.tsx
@@ -1,6 +1,32 @@
 import React, { createContext } from 'react'
 
-export const theme = {}
+const spacingUnit = 8
+
+export const theme = {
+  spacing: {
+    sm: spacingUnit / 2,
+    md: spacingUnit,
+    lg: spacingUnit * 2,
+    xl: spacingUnit * 3,
+    xxl: spacingUnit * 4
+  },
+  fontSize: {
+    sm: spacingUnit,
+    md: spacingUnit * 1.5,
+    regular: spacingUnit * 2,
+    lg: spacingUnit * 3,
+    xl: spacingUnit * 3.5,
+    header: spacingUnit * 4,
+  },
+  colors: {
+    background: '#fff',
+    primary: '#6653ff',
+    secondary: '#697592',
+    lightText: '#586585',
+    blueLight: '#87a5c0',
+    highlight: '#ecf2f5',
+  }
+}
 
 export const ThemeContext = createContext({theme})
 
